e3fd5b628e763fe7cba28803c4195bd72b8cf8cd
Luiz Henrique <luiz.sv96@gmail.com>
2025-04-13 12:41:48 -0300

    remove images, page frontend

--- api/src/app/page.tsx
+++ api/src/app/page.tsx
@@ -1,16 +1,50 @@
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
-import { Form } from "@/components/ui/form";
 import Image from "next/image";
 
 export default function Home() {
   return (
-  <div className="flex flex-col items-center text-center justify-center min-h-screen py-2">
-  <Card>
-    <h5>Qual é o seu perfil de investidor? </h5>
-    <p className="">Você sabe como suas emoções, metas e disposição para correr riscos podem te ajudar a escolher os investimentos mais adequados para você? Participe da nossa pesquisa e dê o primeiro passo para descobrir!</p>
-    <Button className="mb-4">Iniciar o Quiz</Button>
-  </Card>
-  </div>
+    <div className="flex flex-col items-center text-center justify-center min-h-screen py-2">
+      <Card className="w-120 p-12">
+        <h1 className="text-xl font-bold color text-orange-500">Qual é o seu perfil de investidor? </h1>
+        <p className="text-sm text-left">Você sabe como suas emoções, metas e disposição para correr riscos podem te ajudar a escolher os investimentos mais adequados para você?
+          <br /><b>Participe da nossa pesquisa e dê o primeiro passo para descobrir!</b></p>
+        <Button className="mb-4 bg-orange-400 hover:bg-orange-300">Iniciar o Quiz</Button>
+        <hr />
+        <div className="flex flex-col items-center justify-center">
+          <div>
+            <span className="text-sm font-semibold">REALIZAÇÃO:</span>
+            <div className="m-2">
+              <Image
+                src="copyright/prpqnovo.svg"
+                alt="Investidor"
+                width={150}
+                height={150}
+                className="rounded-lg"
+              />
+            </div>
+          </div>
+          <div>
+            <span className="text-sm font-semibold">APOIO:</span>
+            <div className="flex items-center">
+              <Image
+                src="copyright/cnpq-logo.svg"
+                alt="Investidor"
+                width={150}
+                height={150}
+                className="rounded-lg"
+              />
+              <Image
+                src="copyright/fapemig.svg"
+                alt="Investidor"
+                width={150}
+                height={150}
+                className="rounded-lg"
+              />
+            </div>
+          </div>
+        </div>
+      </Card>
+    </div>
   );
 }
