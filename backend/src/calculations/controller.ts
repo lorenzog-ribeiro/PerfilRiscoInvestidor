ff5a0988e97e73bf2d54be3106604568f9c1a51c
Luiz Henrique <luiz.sv96@gmail.com>
2025-04-26 13:42:38 -0300

    include options for user

--- backend/src/calculations/controller.ts
+++ backend/src/calculations/controller.ts
@@ -2,16 +2,26 @@ import { Request, Response } from "express";
 
 export const winScenario = async (req: Request, res: Response) => {
     try {
-        const { aOption, bOption, scenario } = req.body;
+        const { aOption, bOption, scenario, optionSelected } = req.body;
         const forecast: number[] = [0];
 
         const base = (((aOption * 1 / 2) + (bOption * 1 / 2)) - (0 * 0) / 100);
         var aggregate = base;
         forecast.push(parseFloat(aggregate.toFixed(2)));
 
-        for (var i = 1; i < scenario; i++) {
-            aggregate = aggregate + (base / 2 ** i);
-            forecast.push(parseFloat(aggregate.toFixed(2)));
+        switch (optionSelected) {
+            case ("A"):
+                for (var i = 1; i < scenario; i++) {
+                    aggregate = aggregate + (base / 2 ** i);
+                    forecast.push(parseFloat(aggregate.toFixed(2)));
+                }
+                break;
+            case ("B"):
+                for (var i = 1; i < scenario; i++) {
+                    aggregate = aggregate - (base / 2 ** i);
+                    forecast.push(parseFloat(aggregate.toFixed(2)));
+                }
+                break;
         }
         res.status(200).json({ forecast });
     } catch (error: any) {
@@ -21,16 +31,26 @@ export const winScenario = async (req: Request, res: Response) => {
 
 export const lossSCenario = async (req: Request, res: Response) => {
     try {
-        const { aOption, bOption, scenario } = req.body;
+        const { aOption, bOption, scenario, optionSelected } = req.body;
         const forecast: number[] = [0];
 
-        const base = (((aOption * 100) + (aOption * 0)) - (bOption * 1 / 2) / (1 / 2)) * -1;
+        const base = (((aOption * 100) + (aOption * 0)) - (bOption * 1 / 2) / (1 / 2));
         var aggregate = base;
+        forecast.push(parseFloat(aggregate.toFixed(2)));
 
-        for (var i = 1; i < scenario; i++) {
-            aggregate = aggregate + (base / 2 ** i);
-            console.log(aggregate);
-            forecast.push(parseFloat(aggregate.toFixed(2)));
+        switch (optionSelected) {
+            case ("B"):
+                for (var i = 1; i < scenario; i++) {
+                    aggregate = aggregate + (base / 2 ** i);
+                    forecast.push(parseFloat(aggregate.toFixed(2)));
+                }
+                break;
+            case ("A"):
+                for (var i = 1; i < scenario; i++) {
+                    aggregate = aggregate - (base / 2 ** i);
+                    forecast.push(parseFloat(aggregate.toFixed(2)));
+                }
+                break;
         }
         res.status(200).json({ forecast });
     } catch (error: any) {
@@ -39,16 +59,28 @@ export const lossSCenario = async (req: Request, res: Response) => {
 
 export const onlyLossScenario = async (req: Request, res: Response) => {
     try {
-        const { aOption, bOption, scenario } = req.body;
+        const { aOption, bOption, scenario, optionSelected } = req.body;
         const forecast: number[] = [0];
 
         const base = (((aOption * 1 / 2) + (bOption * 1 / 2)) - (0 * 0) / 100);
         var aggregate = base;
+        forecast.push(parseFloat(aggregate.toFixed(2)));
 
-        for (var i = 1; i < scenario; i++) {
-            aggregate = aggregate + (base / 2 ** i);
-            forecast.push(parseFloat(aggregate.toFixed(2)));
+        switch (optionSelected) {
+            case ("A"):
+                for (var i = 1; i < scenario; i++) {
+                    aggregate = aggregate + (base / 2 ** i);
+                    forecast.push(parseFloat(aggregate.toFixed(2)));
+                }
+                break;
+            case ("B"):
+                for (var i = 1; i < scenario; i++) {
+                    aggregate = aggregate - (base / 2 ** i);
+                    forecast.push(parseFloat(aggregate.toFixed(2)));
+                }
+                break;
         }
+
         res.status(200).json({ forecast });
     } catch (error: any) {
 
