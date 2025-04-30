3058acb4047ec02eb4853f78e8fd072f59549eff
Lorenzo Ribeiro <lorenzog.ribeiro2@gmail.com>
2025-04-30 14:12:24 -0300

    http request erro

--- api/services/Axios.tsx
+++ api/services/Axios.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const AxiosInstance = axios.create({
-    baseURL: "https://perfil-investidor-backend-rqabdl-234cf1-137-131-166-114.traefik.me/"
+    baseURL: "http://perfil-investidor-backend-rqabdl-234cf1-137-131-166-114.traefik.me/"
 });
