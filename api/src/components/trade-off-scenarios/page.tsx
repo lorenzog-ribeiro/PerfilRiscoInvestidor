583d6d13c00b31c52fd868d55be9f2d992fd1696
Luiz Henrique <luiz.sv96@gmail.com>
2025-05-01 11:27:32 -0300

    fix router redirect

--- api/src/components/trade-off-scenarios/page.tsx
+++ api/src/components/trade-off-scenarios/page.tsx
@@ -1,22 +1,30 @@
 "use client";
 
-import { redirect } from "next/navigation";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 import FirstScenario from "../first-scenario/page";
 import SecondScenario from "../second-scenario/page";
 import ThirdScenario from "../third-scenario/page";
-import { useState } from "react";
 
 export default function ScenarioController() {
-    const [currentIndex, setCurrentIndex] = useState(0);
-    if(currentIndex == 3) {
-        redirect("/form-questions");
+  const router = useRouter();
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    if (currentIndex === 3) {
+      router.push("/form-questions");
     }
+  }, [currentIndex, router]);
 
-    const scenarios = [
-        <FirstScenario key="1" onAnswered={() => setCurrentIndex(1)} />,
-        <SecondScenario key="2" onAnswered={() => setCurrentIndex(2)} />,
-        <ThirdScenario key="3" onAnswered={() => setCurrentIndex(3)} />,
-    ];
+  const scenarios = [
+    <FirstScenario key="1" onAnswered={() => setCurrentIndex(1)} />,
+    <SecondScenario key="2" onAnswered={() => setCurrentIndex(2)} />,
+    <ThirdScenario key="3" onAnswered={() => setCurrentIndex(3)} />,
+  ];
 
-    return <div className="flex flex-col items-center justify-center space-y-6">{scenarios[currentIndex]}</div>;
+  return (
+    <div className="flex flex-col items-center justify-center space-y-6">
+      {scenarios[currentIndex]}
+    </div>
+  );
 }
