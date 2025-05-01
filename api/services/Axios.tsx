726ead10b3c3d485d335552e4341cdfdbf88808f
Luiz Henrique <luiz.sv96@gmail.com>
2025-05-01 07:31:32 -0300

    add  .env

--- api/services/Axios.tsx
+++ api/services/Axios.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const AxiosInstance = axios.create({
-    baseURL: "http://perfil-investidor-backend-rqabdl-234cf1-137-131-166-114.traefik.me/"
+    baseURL: process.env.NEXT_PUBLIC_BASE_URL
 });
