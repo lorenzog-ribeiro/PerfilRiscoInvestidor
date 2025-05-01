b8740546abe0287c2657542bf75ce4702e43639d
Luiz Henrique <luiz.sv96@gmail.com>
2025-05-01 07:50:39 -0300

    alter connection

--- api/services/Axios.tsx
+++ api/services/Axios.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const AxiosInstance = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_BASE_URL
+    baseURL: process.env.NEXT_PUBLIC_BASE_URL ?? "https://perfil-investidor-backend-rqabdl-234cf1-137-131-166-114.traefik.me/"
 });
