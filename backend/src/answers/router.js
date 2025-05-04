ec16b1a3df5b7d414fc2d832bc2f14c9d9b72ff3
Luiz Henrique <luiz.sv96@gmail.com>
2025-05-04 11:10:17 -0300

    remove .js for src

--- backend/src/answers/router.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const controller_1 = require("./controller");
-const routerAnswers = (0, express_1.Router)();
-routerAnswers.post("/save-answer", controller_1.saveAnswers);
-exports.default = routerAnswers;
