c26c7e0cb656c14dd634b4f1a18844eaccc2c733
Luiz Henrique <luiz.sv96@gmail.com>
2025-05-22 21:30:38 -0300

    alter text

--- api/src/app/(form)/finance-questions/page.tsx
+++ api/src/app/(form)/finance-questions/page.tsx
@@ -8,7 +8,7 @@ export default function FinanceQuestions() {
         <div className="min-h-screen flex items-center justify-center px-4 py-10 bg-[#f1f5f9]">
             <div className="w-full max-w-md bg-white rounded-2xl shadow-md space-y-6">
                 <div className="w-full max-w-5xl space-y-6 flex flex-col items-center justify-center">
-                    <h2 className="text-2xl font-bold text-center text-gray-900 p-3">Como você lida com ganhos</h2>
+                    <h2 className="text-2xl font-bold text-center text-gray-900 p-3">Como você lidaria...</h2>
                     <h3 className="text-1xl font-semibold text-center text-gray-900 p-3">
                         Imagine que você realizou um investimento e está verificando como ele está se saindo; com qual
                         dos cenários a seguir você se sentiria mais confortável?
