2c7e70b6d006b8fb1b3f34853610e6438d163244
Luiz Henrique <luiz.sv96@gmail.com>
2025-05-23 11:45:27 -0300

    fix error

--- backend/dist/calculations/service.js
+++ backend/dist/calculations/service.js
@@ -183,9 +183,9 @@ const result = async (data) => {
         resultThird = ((Number(firstThirdStage?.mediana) / (Number(lastThirdStage?.mediana))));
     }
     else {
-        resultThird = 0;
+        resultThird = 0.01;
     }
-    const result = resultThird !== 0 ? (resultThird / resultFirst) : 0;
+    const result = (resultThird / resultFirst);
     return getProfile({ indice: result, perda: resultThird, ganho: resultFirst, usuario: await (0, repository_2.getbyId)(data.userId) });
 };
 exports.result = result;
--- backend/src/calculations/service.ts
+++ backend/src/calculations/service.ts
@@ -204,9 +204,9 @@ export const result = async (data: any) => {
         resultThird = ((Number(firstThirdStage?.mediana) / (Number(lastThirdStage?.mediana))));
     }
     else {
-        resultThird = 0;
+        resultThird = 0.01;
     }
-    const result = resultThird !== 0 ? (resultThird / resultFirst) : 0;
+    const result = (resultThird / resultFirst);
     return getProfile({ indice: result, perda: resultThird, ganho: resultFirst, usuario: await getbyId(data.userId) });
 }
 
