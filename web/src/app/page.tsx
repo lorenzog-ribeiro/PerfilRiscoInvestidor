aa863b662b27e6f11c3abdd64a393638e8ce6a4d
Lorenzo Ribeiro <lorenzog.ribeiro2@gmail.com>
2025-10-04 09:27:59 -0300

    Implement code changes to enhance functionality and improve performance

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,54 +1,52 @@
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "../components/ui/button";
-import { Card } from "../components/ui/card";
+import { Card, CardHeader } from "../components/ui/card";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center text-center justify-center min-h-screen py-2">
-      <Card className="w-full max-w-md p-6 sm:p-12">
-        <h1 className="text-xl font-bold color text-orange-500">
-          Qual é o seu perfil de investidor?{" "}
-        </h1>
-        <p className="text-sm text-left">
-          Você sabe como suas emoções, metas e disposição para correr riscos
-          podem te ajudar a escolher os investimentos mais adequados para você?
-          <br />
-          <b>
-            Participe da nossa pesquisa e dê o primeiro passo para descobrir!
-          </b>
-        </p>
-        <Button className="mb-4 bg-orange-400 hover:bg-orange-300" asChild>
-          <Link href="/user">Iniciar Quiz</Link>
-        </Button>
+      <Card className="w-full max-w-2xl p-6 sm:p-12">
+        <CardHeader className="mb-4 flex flex-col justify-center items-center">
+          <h1 className="text-xl font-bold color text-blue-800 mb-4">
+            Qual é o seu perfil de investidor?
+          </h1>
+          <p className="text-sm text-left">
+            Você sabe como suas emoções, metas e disposição para correr riscos
+            podem te ajudar a escolher os investimentos mais adequados para você?
+            <br />
+            <b>
+              Participe da nossa pesquisa e dê o primeiro passo para descobrir!
+            </b>
+          </p>
+          <Button className="mt-4 bg-blue-700 hover:bg-blue-500 w-55" asChild>
+            <Link href="/user">Iniciar Quiz</Link>
+          </Button>
+        </CardHeader>
         <hr />
         <div className="flex flex-col items-center justify-center">
           <div>
             <span className="text-sm font-semibold">REALIZAÇÃO:</span>
             <div className="m-2">
               <Image
-                src="copyright/prpqnovo.svg"
+                src="copyright/sof/realizacao.svg"
                 alt="Investidor"
-                width={150}
+                width={800}
                 height={150}
                 className="rounded-lg"
               />
             </div>
           </div>
           <div>
-            <span className="text-sm font-semibold">APOIO:</span>
+            <div className="flex justify-between w-72 mb-2">
+              <span className="text-sm font-semibold">Patrocinio:</span>
+              <span className="text-sm font-semibold">APOIO:</span>
+            </div>
             <div className="flex items-center">
               <Image
-                src="copyright/cnpq-logo.svg"
+                src="copyright/sof/apoiadores.svg"
                 alt="Investidor"
-                width={150}
-                height={150}
-                className="rounded-lg"
-              />
-              <Image
-                src="copyright/fapemig.svg"
-                alt="Investidor"
-                width={150}
+                width={700}
                 height={150}
                 className="rounded-lg"
               />
