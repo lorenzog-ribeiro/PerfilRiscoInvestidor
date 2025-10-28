5ca1f0b1ab7d5a9b1bb6db73c1f55b3ad6cf1717
Lorenzo Ribeiro <lorenzog.ribeiro2@gmail.com>
2025-10-28 20:07:26 -0300

    fix: update SVG attributes to camelCase for consistency

--- web/src/components/instructions/instructions-tradeoff/page.tsx
+++ web/src/components/instructions/instructions-tradeoff/page.tsx
@@ -59,9 +59,9 @@ export default function InstructionsScreen({
                   xmlns="http://www.w3.org/2000/svg"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
                     d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
                   ></path>
                 </svg>
