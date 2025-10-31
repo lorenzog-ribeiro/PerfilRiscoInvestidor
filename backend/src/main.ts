5327d13722f20031d885ef1574766ae3cb42c7d9
Luiz Henrique <55117003+luxrail@users.noreply.github.com>
2025-10-31 19:47:51 -0300

    fix error cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -13,6 +13,7 @@ async function bootstrap() {
     'https://devsof.duodevs.com.br', // Produção frontend
     'https://perfilderiscosof.duodevs.com.br',
     'https://perfil.spaceorion.com.br',
+    'https://perfilderisco.duodevs.com.br',
     process.env.FRONTEND_URL, // Dinâmico
   ].filter(Boolean); // Remove undefined
 
